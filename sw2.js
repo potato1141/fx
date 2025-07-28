a152f3e1edacec317824ecb07d87f1a5b8d63648
Xtra <nooripouria386@gmail.com>
2025-07-28 12:05:29 +0330

    Add files via upload

--- sw2.js
+++ sw2.js
@@ -1,4 +1,4 @@
-const cacheName = "1753084714349"; // this gets replaced by the build script
+const cacheName = "1753688015708"; // this gets replaced by the build script
 
 self.addEventListener("install", (e) => {
   console.log("[Service Worker] Install", cacheName);
